34bb62eebe782ea78fac59d2a7d9abed129e3c85
NaNkotsukan <nankotsukan@gmail.com>
2019-02-12 20:59:01 +0900

    LU分解の実装

---
+++ main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+typedef double dtype;
+const int dsize = sizeof(dtype);
+
+void showArray(dtype *array, int N){
+    for(int i=0;i<N;++i){
+        for(int j=0;j<N;++j){
+            printf("% 5lf ", array[N*i+j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
+void dot(dtype *a, dtype *b, dtype *c, int N){
+    for(int i=0;i<N;++i){
+        for(int j=0;j<N;++j){
+            dtype hoge = 0;
+            for(int k = 0; k < N; ++k)
+            {
+                hoge+=a[i*N+k]*b[k*N+j];
+            }
+            c[i*N+j]=hoge;
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int N;
+    scanf("%d", &N);
+    dtype* A = (dtype*)calloc(N*N, dsize);
+    dtype* L = (dtype*)calloc(N*N, dsize);
+    dtype* U = (dtype*)calloc(N*N, dsize);
+
+    if(A==NULL||L==NULL||U==NULL){
+        printf("memory error\n");
+        return 0;
+    }
+
+    double t;
+    for(int i = 0; i < N*N ; ++i){
+        scanf("%lf", &t);
+        A[i] = (dtype)t;
+    }
+
+    for(int i = 0; i < N*N; i += N + 1){
+    }
+    memcpy(U, A, dsize*N);
+    L[0] = (dtype)1;
+
+    for(int i = 1; i < N; ++i)
+    {
+        L[i*(N+1)] = (dtype)1;
+
+        for(int j = 0; j < N; ++j)
+        {
+            if(j < i){
+                dtype l = (dtype)0;
+                for(int k = 0; k < j; k++)
+                {
+                    l+=L[i*N+k]*U[k*N+j];
+                }
+                L[i*N+j] = (A[i*N+j]-l)/U[j*(N+1)];
+            }
+            else
+            {
+                dtype u = (dtype)0;
+                for(int k = 0; k < i; k++)
+                {
+                    u+=L[i*N+k]*U[k*N+j];
+                }
+                U[i*N+j] = (A[i*N+j]-u);
+            }
+
+        }
+        
+    }
+
+    showArray(A, N);
+    showArray(L, N);
+    showArray(U, N);
+    
+    dtype* test = (dtype*)calloc(N*N, dsize);
+    dot(L, U, test, N);
+    showArray(test, N);
+    
+    return 0;
+}
